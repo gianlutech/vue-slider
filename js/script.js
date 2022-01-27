8ca3ee817cfadbbc0205789fa03a87021d5974c2
gianlutech <gianluca.ingrao95@gmail.com>
2022-01-27 15:51:08 +0100

    start

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 console.log('JS OK');
+console.log('Vue OK', Vue);
+
+/* 
+Partendo dal markup fornito in allegato, implementare la logica per far funzionare lo slider:
+Deve essere possibile scorrere le immagini cliccando sulle freccette.
+I pallini in basso devono illuminarsi in base all'immagine che sto visualizzando.
+Far si che cliccando i pallini, venga selezionata l'immagine corrispondente.
+Note:  scegliamo una delle due seguenti soluzioni per le freccette:
+Se stiamo visualizzando la prima immagine, la freccetta di sinistra può:
+scomparire
+passare all'ultima immagine
+Se stiamo visualizzando l'ultima immagine, la freccetta  a destra può:
+scomparire
+passare alla prima immagine
+Siate coerenti: o scompaiono o continuano a navigare, no a soluzioni miste!!
+*/

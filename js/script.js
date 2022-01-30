5041035724831195a4917cd17f92955025c95ae8
gianlutech <gianluca.ingrao95@gmail.com>
2022-01-30 16:39:20 +0100

    completed

--- js/script.js
+++ js/script.js
@@ -19,11 +19,32 @@ Siate coerenti: o scompaiono o continuano a navigare, no a soluzioni miste!!
 const root = new Vue({
     el: '#root',
     data: {
+        currentIndex: 0,
         images: [
             'images/image1.jpg',
             'images/image2.jpg',
             'images/image3.jpg',
             'images/image4.jpg',
         ]
+    },
+    methods: {
+        isActive(index) {
+            return index === this.currentIndex;
+        },
+
+        setPic(index) {
+            this.currentIndex = index;
+        },
+
+        prevPic() {
+            if (this.currentIndex === 0) this.currentIndex = this.images.length - 1;
+            else this.currentIndex--;
+        },
+
+        nextPic() {
+            if (this.currentIndex === this.images.length - 1) this.currentIndex = 0;
+            else this.currentIndex++;
+        },
     }
-})
+
+});
